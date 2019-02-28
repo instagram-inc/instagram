c8bf287d2e88ad70579c6c47f230b713694c71a8
AlexVaglarov <axell92@abv.bg>
2019-02-28 18:12:11 +0200

    ActivityIcons Component created

--- instagram/src/App/App.js
+++ instagram/src/App/App.js
@@ -4,12 +4,13 @@ import FollowButton from '../common/UI/FollowButton/FollowButton';
 import ArticleContainer from '../common/UI/ArticleContainer/ArticleContainer';
 import CircleImg from '../common/UI/CircleImg/CircleImg';
 import InstaHeader from '../Header/Header'
+import ActivityIcons from '../common/UI/ActivityIcons/ActivityIcons'
  
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Test</h1>
+        {/* <h1>Test</h1> */}
 
         <InstaHeader />
 
@@ -17,6 +18,8 @@ class App extends Component {
         <FollowButton />
         <CircleImg src={"https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} 
         width = {500}/>
+
+        <ActivityIcons />
         
       </div>
     );
